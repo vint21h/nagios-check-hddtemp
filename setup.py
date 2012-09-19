11ae273f9671c2995c8450dba132149df6b9c537
vINT 21h <madreaper@gmail.com>
2012-09-19 13:26:17 +0300

    new refactor iteration, rename and version bump

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# check_hddtemp
+# nagios-check-hddtemp
 # setup.py
 
-import os
-import warnings
 from setuptools import setup, find_packages
 
 from check_hddtemp import (
@@ -17,24 +15,24 @@ from check_hddtemp import (
     __url__,
 )
 
-SHARE_FILES = ['README.rst', 'COPYING', ]
+SHARED_FILES = ['README.rst', 'COPYING', ]
 
 setup(
-    name = "check_hddtemp",
+    name = "nagios_check_hddtemp",
     version = __version__,
     packages = find_packages(),
     scripts = ['check_hddtemp.py', ],
     install_requires = ['docutils', ],
     package_data = {
-        '': SHARE_FILES,
+        '': SHARED_FILES,
     },
     data_files = [
-            ('/usr/share/doc/check_hddtemp/', SHARE_FILES),
+            ('/usr/share/doc/nagios-check-hddtemp/', SHARED_FILES),
     ],
     author = __author__,
     author_email = __email__,
     description = __description__,
-    long_description = __description__,
+    long_description = open('README.rst').read(),
     license = __licence__,
     url = __url__,
     zip_safe = False,
