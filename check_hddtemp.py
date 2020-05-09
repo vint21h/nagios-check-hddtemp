1b40f64b815b69ce2e9a69f92b390a83aac3c04a
Alexei Andrushievich <vint21h@vint21h.pp.ua>
2020-05-10 01:35:50 +0300

    Version up

--- check_hddtemp.py
+++ check_hddtemp.py
@@ -40,7 +40,7 @@ __all__ = [
 
 
 # metadata
-VERSION = (1, 1, 11)
+VERSION = (1, 2, 0)
 __version__ = ".".join(map(str, VERSION))
 
 
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = (1, 1, 11)
+VERSION = (1, 2, 0)
 __version__ = ".".join(map(str, VERSION))
 
 DATA = [
